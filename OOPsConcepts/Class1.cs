ea133ce9a0ddb95e88b2cd921376ef6336d65e7d
AnilBamne <anilbamne20@gmail.com>
2022-12-11 14:15:52 +0530

    Adding:Inheritance code

---
+++ OOPsConcepts/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConcepts
+{
+    public class Class1     // Parent class
+    {
+        public void Method1()
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("Running method 1 from Class1");
+            Console.WriteLine("Sum is = "+(a+b));
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Running method 2 from Class1");
+        }
+    }
+
+    public class Class2 : Class1     //child class -->Inheriting Class1 properties in Class2
+    {
+        public void method3()
+        {
+            Class2 c2 = new Class2();
+            c2.Method1();
+            c2.Method2();
+        }
+    }
+}
--- OOPsConcepts/Program.cs
+++ OOPsConcepts/Program.cs
@@ -3,11 +3,24 @@ using System;
 
 namespace OOPsConcepts
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To OOPs concepts");
+            Console.WriteLine("Choose option \n1:Inheritance\n2:Abstraction\n");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1://Inheritance
+                    Class2 c = new Class2();
+                    c.method3();
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
