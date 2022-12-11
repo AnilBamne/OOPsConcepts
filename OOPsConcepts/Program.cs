585ec768b055c5e98b8a1c58d1b606c6eba4aeb3
AnilBamne <anilbamne20@gmail.com>
2022-12-11 13:15:34 +0530

    Addng:WelcomeMessage

---
+++ OOPsConcepts/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace OOPsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To OOPs concepts");
+        }
+    }
+}
